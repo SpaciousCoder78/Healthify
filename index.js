1ccd7508fa721ffa0beb9c3453dab4dbc1e32f68
Lonelyguy123 <86297324+Lonelyguy123@users.noreply.github.com>
2023-07-13 23:39:01 +0530

    timer

--- index.js
+++ index.js
@@ -1,196 +1,99 @@
+// Variable dump
 let timer1 = document.querySelector('.timer1');
 let button1 = document.querySelector('.start1');
-let startButton1 = document.createElement("button");
-//########################Timer 2#######################################
-let timer2 = document.querySelector(".timer2");
-let button2= document.querySelector(".start2");
-let startButton2 = document.createElement("button");
-//########################Timer 3#######################################
-let timer3= document.querySelector(".timer3");
-let button3 = document.querySelector(".start3");
-let startButton3 = document.createElement("button");
-//########################Timer 4#######################################
-let timer4=document.querySelector(".timer4");
-let button4= document.querySelector(".start4");
-let startButton4 = document.createElement("button");
-let timerInterval1,timerInterval2,timerInterval3,timerInterval4; // Variable to store the timer interval ID
-//*********************************************************************** */
+let timer2 = document.querySelector('.timer2');
+let button2 = document.querySelector('.start2');
+let timer3 = document.querySelector('.timer3');
+let button3 = document.querySelector('.start3');
+let timer4 = document.querySelector('.timer4');
+let button4 = document.querySelector('.start4');
+let timerInterval1, timerInterval2, timerInterval3, timerInterval4; // Variable to store the timer interval ID
 
-//arrays to define the types of workout
-const chestExercise = ['Pushup', 'Bench Press', 'Incline Dumbell Press', 'Dumbel Flys', 'Dips'];
+// Arrays to define the types of workouts
+const chestExercise = ['Pushups', 'Bench Press', 'Incline Dumbbell Press', 'Dumbbell Flys', 'Dips'];
 const backExercise = ['Pull-ups', 'Deadlifts', 'Bent Over Rows', 'Lat Pulldowns', 'Seated Cable Rows'];
-const legExercise = ['Squats', 'Lunges', 'Deadlifts', 'Leg Press', 'Calf Raises'];
-const absExercise = ['Crunches', 'Plank', 'Russian Twists', 'Leg Raises', 'Bicycle Crunches'];
 const shoulderExercise = ['Overhead Press', 'Dumbbell Shoulder Press', 'Arnold Press', 'Lateral Raises', 'Front Raises'];
+const legExercise = ['Squats', 'Lunges', 'Deadlifts', 'Leg Press', 'Calf Raises'];
 
-//************************************************************************ */
-//action listeners to make the buttons actually work
-//############################button 1##############################
-button1.addEventListener("click", () => {
+// Action listeners to make the buttons work
+button1.addEventListener('click', startWorkout.bind(null, chestExercise, timer1, button1));
+button2.addEventListener('click', startWorkout.bind(null, backExercise, timer2, button2));
+button3.addEventListener('click', startWorkout.bind(null, shoulderExercise, timer3, button3));
+button4.addEventListener('click', startWorkout.bind(null, legExercise, timer4, button4));
+
+// Function to start the workout
+function startWorkout(exercises, timerElement, buttonElement) {
+  let exerciseIndex = 0;
   let duration = 45; // Duration in seconds
-  let minutes, seconds;
 
   // Clear any existing timer interval
   clearInterval(timerInterval1);
-
-  timerInterval1 = setInterval(() => {
-    minutes = parseInt(duration / 60, 10);
-    seconds = parseInt(duration % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    timer1.innerHTML = minutes + ":" + seconds;
-
-    if (--duration < 0) {
-      clearInterval(timerInterval1);
-
-      // Create a new start button element
-      
-      startButton1.innerText = "Start";
-      startButton1.classList.add("start1");
-
-      // Replace the timer element with the start button
-      timer1.replaceWith(startButton1);
-
-      // Add event listener to the new start button
-      startButton1.addEventListener("click", startButtonClick1);
-
-    }
-  }, 1000);
-});
-
-//function to make the start button work again
-function startButtonClick1() {
-  // Restart the timer by triggering the button click
-  button1.click();
-  startButton1.replaceWith(timer1);
-}
-//***************************************************************************** */
-//############################Button 2#########################
-button2.addEventListener("click", () => {
-  let duration = 45; // Duration in seconds
-  let minutes, seconds;
-
-  // Clear any existing timer interval
   clearInterval(timerInterval2);
-
-  timerInterval2 = setInterval(() => {
-    minutes = parseInt(duration / 60, 10);
-    seconds = parseInt(duration % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    timer2.innerHTML = minutes + ":" + seconds;
-
-    if (--duration < 0) {
-      clearInterval(timerInterval2);
-
-      // Create a new start button element
-      
-      startButton2.innerText = "Start";
-      startButton2.classList.add("start2");
-
-      // Replace the timer element with the start button
-      timer2.replaceWith(startButton2);
-
-      // Add event listener to the new start button
-      startButton2.addEventListener("click", startButtonClick2);
-
-    }
-  }, 1000);
-});
-
-//function to make the start button work again
-function startButtonClick2() {
-  // Restart the timer by triggering the button click
-  button2.click();
-  startButton2.replaceWith(timer2);
-}
-
-//******************************************************************************************************** */
-//###############################Button 3##################################
-button3.addEventListener("click", () => {
-  let duration = 45; // Duration in seconds
-  let minutes, seconds;
-
-  // Clear any existing timer interval
   clearInterval(timerInterval3);
-
-  timerInterval3 = setInterval(() => {
-    minutes = parseInt(duration / 60, 10);
-    seconds = parseInt(duration % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    timer3.innerHTML = minutes + ":" + seconds;
-
-    if (--duration < 0) {
-      clearInterval(timerInterval3);
-
-      // Create a new start button element
-      
-      startButton3.innerText = "Start";
-      startButton3.classList.add("start3");
-
-      // Replace the timer element with the start button
-      timer3.replaceWith(startButton3);
-
-      // Add event listener to the new start button
-      startButton3.addEventListener("click", startButtonClick3);
-
-    }
-  }, 1000);
-});
-
-//function to make the start button work again
-function startButtonClick3() {
-  // Restart the timer by triggering the button click
-  button3.click();
-  startButton3.replaceWith(timer3);
-}
-
-//******************************************************************************************** */
-//##############################Button 4##############################
-button4.addEventListener("click", () => {
-  let duration = 45; // Duration in seconds
-  let minutes, seconds;
-
-  // Clear any existing timer interval
   clearInterval(timerInterval4);
 
-  timerInterval4 = setInterval(() => {
-    minutes = parseInt(duration / 60, 10);
-    seconds = parseInt(duration % 60, 10);
+  // Function to update the timer display
+  const updateTimer = () => {
+    let minutes = parseInt(duration / 60, 10);
+    let seconds = parseInt(duration % 60, 10);
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    timer4.innerHTML = minutes + ":" + seconds;
+    timerElement.textContent = minutes + ':' + seconds;
+  };
 
-    if (--duration < 0) {
-      clearInterval(timerInterval4);
+  // Function to start the next exercise
+  const startNextExercise = () => {
+    // Increment exercise index
+    exerciseIndex++;
 
-      // Create a new start button element
-      
-      startButton4.innerText = "Start";
-      startButton4.classList.add("start4");
+    if (exerciseIndex < exercises.length) {
+      // Update exercise name for the next exercise
+      timerElement.previousElementSibling.textContent = exercises[exerciseIndex];
 
-      // Replace the timer element with the start button
-      timer4.replaceWith(startButton4);
+      // Reset the duration for the next exercise
+      duration = 45;
 
-      // Add event listener to the new start button
-      startButton4.addEventListener("click", startButtonClick4);
-
+      // Start the timer for the next exercise after a 5-second delay
+      setTimeout(startTimer, 5000);
+    } else {
+      // Reset the timer and show "Start" button
+      timerElement.textContent = 'Start';
+      buttonElement.disabled = false;
     }
-  }, 1000);
-});
+  };
 
-//function to make the start button work again
-function startButtonClick4() {
-  // Restart the timer by triggering the button click
-  button4.click();
-  startButton4.replaceWith(timer4);
+  // Function to start the timer for the current exercise
+  const startTimer = () => {
+    updateTimer();
+
+    // Decrement duration
+    duration--;
+
+    if (duration < 0) {
+      // Start the next exercise
+      startNextExercise();
+    }
+  };
+
+  // Disable the button to prevent multiple clicks
+  buttonElement.disabled = true;
+
+  // Update exercise name for the first exercise
+  timerElement.previousElementSibling.textContent = exercises[exerciseIndex];
+
+  // Start the timer
+  startTimer();
+
+  // Set the timer interval for the workout
+  if (timerElement === timer1) {
+    timerInterval1 = setInterval(startTimer, 1000);
+  } else if (timerElement === timer2) {
+    timerInterval2 = setInterval(startTimer, 1000);
+  } else if (timerElement === timer3) {
+    timerInterval3 = setInterval(startTimer, 1000);
+  } else if (timerElement === timer4) {
+    timerInterval4 = setInterval(startTimer, 1000);
+  }
 }
